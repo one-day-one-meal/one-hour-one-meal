afd6d4c539aef19a7509e4dfec77e54901d2ab9e
ckhcree <kbyo0324@gmail.com>
2024-06-12 18:05:30 +0900

    :bug: Comment 디티오 수정

--- src/main/kotlin/team/sparta/onehouronemeal/domain/comment/dto/v1/CreateCommentRequest.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/comment/dto/v1/CreateCommentRequest.kt
@@ -1,5 +1,6 @@
 package team.sparta.onehouronemeal.domain.comment.dto.v1
 
 data class CreateCommentRequest(
+    val courseId: Long,
     val content: String
 )
--- src/main/kotlin/team/sparta/onehouronemeal/domain/comment/dto/v1/UpdateCommentRequest.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/comment/dto/v1/UpdateCommentRequest.kt
@@ -1,5 +1,6 @@
 package team.sparta.onehouronemeal.domain.comment.dto.v1
 
 class UpdateCommentRequest(
+    val courseId: Long,
     val content: String
 )
