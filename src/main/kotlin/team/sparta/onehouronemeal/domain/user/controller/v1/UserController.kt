8d2b59ce5579e507e842383d4476e987c46fc619
Jaewon Noh <opqw987@gmail.com>
2024-06-12 19:07:20 +0900

    :art: SecurityUtils 삭제 후 Controller 에서 UserPrincipal 참조

--- src/main/kotlin/team/sparta/onehouronemeal/domain/user/controller/v1/UserController.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/user/controller/v1/UserController.kt
@@ -3,6 +3,7 @@ package team.sparta.onehouronemeal.domain.user.controller.v1
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,6 +18,7 @@ import team.sparta.onehouronemeal.domain.user.dto.v1.TokenCheckResponse
 import team.sparta.onehouronemeal.domain.user.dto.v1.UpdateUserRequest
 import team.sparta.onehouronemeal.domain.user.dto.v1.UserResponse
 import team.sparta.onehouronemeal.domain.user.service.v1.UserService
+import team.sparta.onehouronemeal.infra.security.UserPrincipal
 
 @RestController
 @RequestMapping("/api/v1")
@@ -59,10 +61,14 @@ class UserController(
     }
 
     @GetMapping("/auth/token-check")
-    fun tokenTestCheck(httpServlet: HttpServletRequest): ResponseEntity<TokenCheckResponse> {
+    fun tokenTestCheck(
+        @AuthenticationPrincipal principal: UserPrincipal,
+        httpServlet: HttpServletRequest
+    ): ResponseEntity<TokenCheckResponse> {
         val accessToken = httpServlet.getHeader("Authorization")
             ?: throw IllegalArgumentException("Authorization header is required")
 
-        return ResponseEntity.status(HttpStatus.OK).body(userService.tokenTestCheck(accessToken))
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(userService.tokenTestCheck(accessToken = accessToken, principal = principal))
     }
 }
