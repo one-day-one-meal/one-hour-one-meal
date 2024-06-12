b3e1746f09089e4db84890c48aa08895eb0c61b3
1hyung <ryu1hyung@naver.com>
2024-06-12 20:03:41 +0900

    :sparkles: Feat: Recipe CRUD 구현

--- src/main/kotlin/team/sparta/onehouronemeal/domain/recipe/repository/v1/RecipeRepository.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/recipe/repository/v1/RecipeRepository.kt
@@ -1,7 +1,11 @@
 package team.sparta.onehouronemeal.domain.recipe.repository.v1
 
 import org.springframework.data.jpa.repository.JpaRepository
+import team.sparta.onehouronemeal.domain.course.model.v1.Course
 import team.sparta.onehouronemeal.domain.recipe.model.v1.Recipe
+import java.util.Optional
 
-interface RecipeRepository : JpaRepository<Recipe, Long>, CustomRecipeRepository
-// 나중에 Jpa이름으로 쿼리 작성할 때 이곳에 작성
+interface RecipeRepository : JpaRepository<Recipe, Long>, CustomRecipeRepository {
+    fun findByCourse(course: Course): List<Recipe>
+    fun findByCourseAndId(course: Course, id: Long): Optional<Recipe>
+}
--- src/main/kotlin/team/sparta/onehouronemeal/domain/recipe/service/v1/RecipeService.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/recipe/service/v1/RecipeService.kt
@@ -1,32 +1,62 @@
 package team.sparta.onehouronemeal.domain.recipe.service.v1
 
 import org.springframework.stereotype.Service
+import team.sparta.onehouronemeal.domain.course.repository.v1.CourseRepository
 import team.sparta.onehouronemeal.domain.recipe.dto.v1.CreateRecipeRequest
 import team.sparta.onehouronemeal.domain.recipe.dto.v1.RecipeResponse
 import team.sparta.onehouronemeal.domain.recipe.dto.v1.UpdateRecipeRequest
+import team.sparta.onehouronemeal.domain.recipe.model.v1.Recipe
 import team.sparta.onehouronemeal.domain.recipe.repository.v1.RecipeRepository
 
 @Service
 class RecipeService(
     private val recipeRepository: RecipeRepository,
+    private val courseRepository: CourseRepository,
 ) {
     fun getAllRecipeList(courseId: Long): List<RecipeResponse> {
-        TODO()
+        val course = courseRepository.findById(courseId)
+            .orElseThrow { RuntimeException("Course not found") }
+        return recipeRepository.findByCourse(course).map { RecipeResponse.from(it) }
     }
 
     fun getRecipeById(courseId: Long, recipeId: Long): RecipeResponse {
-        TODO()
+        val course = courseRepository.findById(courseId).orElseThrow { RuntimeException("Course not found") }
+        val recipe =
+            recipeRepository.findByCourseAndId(course, recipeId).orElseThrow { RuntimeException("Recipe not found") }
+        return RecipeResponse.from(recipe)
     }
 
     fun createRecipe(courseId: Long, request: CreateRecipeRequest): RecipeResponse {
-        TODO()
+        val course = courseRepository.findById(courseId)
+            .orElseThrow { RuntimeException("Course not found") }
+        val recipe = Recipe(
+            course = course,
+            title = request.title,
+            describe = request.describe,
+            videoUrl = request.videoUrl
+        )
+        return RecipeResponse.from(recipeRepository.save(recipe))
     }
 
     fun updateRecipe(courseId: Long, recipeId: Long, request: UpdateRecipeRequest): RecipeResponse {
-        TODO()
+        val course = courseRepository.findById(courseId)
+            .orElseThrow { RuntimeException("Course not found") }
+        val recipe = recipeRepository.findByCourseAndId(course, recipeId)
+            .orElseThrow { RuntimeException("Recipe not found") }
+
+        recipe.title = request.title
+        recipe.describe = request.describe
+        recipe.videoUrl = request.videoUrl
+
+        return RecipeResponse.from(recipeRepository.save(recipe))
     }
 
     fun deleteRecipe(courseId: Long, recipeId: Long) {
-        TODO()
+        val course = courseRepository.findById(courseId)
+            .orElseThrow { RuntimeException("Course not found") }
+        val recipe = recipeRepository.findByCourseAndId(course, recipeId)
+            .orElseThrow { RuntimeException("Recipe not found") }
+
+        recipeRepository.delete(recipe)
     }
 }
