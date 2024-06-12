ba12803e7e3b362e0a051bd7be2f5d4749e45595
Jaewon Noh <opqw987@gmail.com>
2024-06-12 16:46:29 +0900

    :sparkles: User Repository 기본 작성

--- src/main/kotlin/team/sparta/onehouronemeal/domain/user/repository/v1/UserRepository.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/user/repository/v1/UserRepository.kt
@@ -4,4 +4,7 @@ import team.sparta.onehouronemeal.domain.user.model.v1.User
 
 interface UserRepository {
     fun findById(id: Long): User?
+    fun findByUsername(username: String): User?
+    fun save(user: User): User
+    fun delete(user: User)
 }
