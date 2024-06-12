0197fa347243d744eda36967d5c265cdebcf1fbf
Jaewon Noh <opqw987@gmail.com>
2024-06-12 09:01:09 +0900

    :sparkles: JJWT를 통한 JWT 발급 추가 및 Spring security filter chain 등록

---
+++ src/main/kotlin/team/sparta/onehouronemeal/infra/security/SecurityConfig.kt
@@ -0,0 +1,45 @@
+package team.sparta.onehouronemeal.infra.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import team.sparta.onehouronemeal.infra.security.jwt.JwtAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val customAuthenticationEntrypoint: CustomAuthenticationEntrypoint,
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers(
+                    // 인증 대상에서 제외할 URL 설정
+                    "/api/v1/auth/**",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                ).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling { it.authenticationEntryPoint(customAuthenticationEntrypoint) }
+            .build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
---
+++ src/main/kotlin/team/sparta/onehouronemeal/infra/security/UserPrincipal.kt
@@ -0,0 +1,14 @@
+package team.sparta.onehouronemeal.infra.security
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+
+data class UserPrincipal(
+    val id: Long,
+    val authorities: Collection<GrantedAuthority>
+) {
+    constructor(id: Long, roles: Set<String>) : this(
+        id,
+        roles.map { SimpleGrantedAuthority("ROLE_$it") }
+    )
+}
