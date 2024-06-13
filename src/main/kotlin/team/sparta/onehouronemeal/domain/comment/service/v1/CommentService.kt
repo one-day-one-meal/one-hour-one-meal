950d3fb39365c9d2c791d8f736c71c033de740d6
ckhcree <kbyo0324@gmail.com>
2024-06-13 17:11:07 +0900

    :bug: Comment 서비스 및 컨트롤러에 Report 기능 추가

--- src/main/kotlin/team/sparta/onehouronemeal/domain/comment/service/v1/CommentService.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/comment/service/v1/CommentService.kt
@@ -7,18 +7,24 @@ import team.sparta.onehouronemeal.domain.auth.common.PermissionChecker
 import team.sparta.onehouronemeal.domain.comment.dto.v1.CommentResponse
 import team.sparta.onehouronemeal.domain.comment.dto.v1.CreateCommentRequest
 import team.sparta.onehouronemeal.domain.comment.dto.v1.UpdateCommentRequest
+import team.sparta.onehouronemeal.domain.comment.dto.v1.report.ReportRequest
+import team.sparta.onehouronemeal.domain.comment.dto.v1.report.ReportResponse
 import team.sparta.onehouronemeal.domain.comment.model.v1.Comment
+import team.sparta.onehouronemeal.domain.comment.model.v1.report.Report
 import team.sparta.onehouronemeal.domain.comment.repository.v1.CommentRepository
+import team.sparta.onehouronemeal.domain.comment.repository.v1.report.ReportRepository
 import team.sparta.onehouronemeal.domain.course.repository.v1.CourseRepository
 import team.sparta.onehouronemeal.domain.user.repository.v1.UserRepository
+import team.sparta.onehouronemeal.exception.AccessDeniedException
 import team.sparta.onehouronemeal.exception.ModelNotFoundException
 import team.sparta.onehouronemeal.infra.security.UserPrincipal
 
 @Service
 class CommentService(
-    private val commentRepository: CommentRepository,
     private val userRepository: UserRepository,
     private val courseRepository: CourseRepository,
+    private val commentRepository: CommentRepository,
+    private val reportRepository: ReportRepository,
 ) {
     fun getComment(commentId: Long): CommentResponse {
         val comment =
@@ -60,12 +66,20 @@ class CommentService(
     }
 
     @Transactional
-    fun reportComment(principal: UserPrincipal, commentId: Long) {
-        // User 본인 확인
-        // 코멘트 아이디를 가지고 코멘트 신고
+    fun reportComment(principal: UserPrincipal, commentId: Long, request: ReportRequest): ReportResponse {
+        val user = userRepository.findById(principal.id) ?: throw ModelNotFoundException("User", principal.id)
+        val comment = commentRepository.findByIdOrNull(commentId) ?: throw ModelNotFoundException("Comment", commentId)
 
-        // 본인 댓글에 신고 안됨
+        if (comment.user.id == principal.id) throw AccessDeniedException("본인 댓글에 신고 못해요.")
+        if (reportRepository.existsByUserIdAndCommentId(principal.id, commentId)) {
+            throw IllegalArgumentException("이미 해당 댓글 신고했어요.")
+        }
 
-        TODO()
+        val report = Report(
+            comment = comment,
+            user = user,
+            description = request.description,
+        )
+        return ReportResponse.from(reportRepository.save(report))
     }
 }
