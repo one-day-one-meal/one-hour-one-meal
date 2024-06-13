9d41bebb51ffa2f3eff8125c9808c020d6359784
Jaewon Noh <opqw987@gmail.com>
2024-06-13 14:40:49 +0900

    :sparkles: Subscription entity 추가

--- src/main/kotlin/team/sparta/onehouronemeal/domain/user/model/v1/subscription/Subscription.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/user/model/v1/subscription/Subscription.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Embeddable
 import jakarta.persistence.EmbeddedId
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.MapsId
 import jakarta.persistence.Table
@@ -18,10 +19,12 @@ class Subscription(
 
     @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("userId")
+    @JoinColumn(name = "user_id")
     val user: User,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("subscribedUserId")
+    @JoinColumn(name = "subscribed_user_id")
     val subscribedUser: User,
 )
 
