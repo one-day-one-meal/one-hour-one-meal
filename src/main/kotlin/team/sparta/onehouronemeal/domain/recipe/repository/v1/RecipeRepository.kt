09dc2a244ceecd5822e9c459dba38cf9a64d054a
1hyung <161712242+1hyung@users.noreply.github.com>
2024-06-13 09:07:33 +0900

    [Feat] Recipe CRUD 구현 (#46)
    ## 요약
    Recipe CRUD 구현했습니다.
    
    ## 작업 사항
    RecipeService, RecipeRepository 구현
    
    ---
    ### 해결한 이슈
    closes: #16

--- src/main/kotlin/team/sparta/onehouronemeal/domain/recipe/repository/v1/RecipeRepository.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/recipe/repository/v1/RecipeRepository.kt
@@ -3,5 +3,7 @@ package team.sparta.onehouronemeal.domain.recipe.repository.v1
 import org.springframework.data.jpa.repository.JpaRepository
 import team.sparta.onehouronemeal.domain.recipe.model.v1.Recipe
 
-interface RecipeRepository : JpaRepository<Recipe, Long>, CustomRecipeRepository
-// 나중에 Jpa이름으로 쿼리 작성할 때 이곳에 작성
+interface RecipeRepository : JpaRepository<Recipe, Long>, CustomRecipeRepository {
+    fun findAllByCourseId(courseId: Long): List<Recipe>
+    fun findByCourseIdAndId(courseId: Long, id: Long): Recipe?
+}
--- src/main/kotlin/team/sparta/onehouronemeal/domain/recipe/service/v1/RecipeService.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/recipe/service/v1/RecipeService.kt
@@ -1,32 +1,63 @@
 package team.sparta.onehouronemeal.domain.recipe.service.v1
 
+import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import team.sparta.onehouronemeal.domain.course.repository.v1.CourseRepository
 import team.sparta.onehouronemeal.domain.recipe.dto.v1.CreateRecipeRequest
 import team.sparta.onehouronemeal.domain.recipe.dto.v1.RecipeResponse
 import team.sparta.onehouronemeal.domain.recipe.dto.v1.UpdateRecipeRequest
+import team.sparta.onehouronemeal.domain.recipe.model.v1.Recipe
 import team.sparta.onehouronemeal.domain.recipe.repository.v1.RecipeRepository
+import team.sparta.onehouronemeal.exception.ModelNotFoundException
 
 @Service
 class RecipeService(
     private val recipeRepository: RecipeRepository,
+    private val courseRepository: CourseRepository,
 ) {
     fun getAllRecipeList(courseId: Long): List<RecipeResponse> {
-        TODO()
+        if (!courseRepository.existsById(courseId)) throw ModelNotFoundException("Course", courseId)
+        return recipeRepository.findAllByCourseId(courseId).map { RecipeResponse.from(it) }
     }
 
     fun getRecipeById(courseId: Long, recipeId: Long): RecipeResponse {
-        TODO()
+        if (!courseRepository.existsById(courseId)) throw ModelNotFoundException("Course", courseId)
+        val recipe =
+            recipeRepository.findByCourseIdAndId(courseId, recipeId) ?: throw ModelNotFoundException("Recipe", recipeId)
+        return RecipeResponse.from(recipe)
     }
 
     fun createRecipe(courseId: Long, request: CreateRecipeRequest): RecipeResponse {
-        TODO()
+        val course = courseRepository.findByIdOrNull(courseId) ?: throw ModelNotFoundException("Course", courseId)
+        val recipe = Recipe(
+            course = course,
+            title = request.title,
+            describe = request.describe,
+            videoUrl = request.videoUrl
+        )
+        return RecipeResponse.from(recipeRepository.save(recipe))
     }
 
+    @Transactional
     fun updateRecipe(courseId: Long, recipeId: Long, request: UpdateRecipeRequest): RecipeResponse {
-        TODO()
+        if (!courseRepository.existsById(courseId)) throw ModelNotFoundException("Course", courseId)
+        val recipe =
+            recipeRepository.findByCourseIdAndId(courseId, recipeId) ?: throw ModelNotFoundException("Recipe", recipeId)
+
+        recipe.title = request.title
+        recipe.describe = request.describe
+        recipe.videoUrl = request.videoUrl
+
+        return RecipeResponse.from(recipe)
     }
 
+    @Transactional
     fun deleteRecipe(courseId: Long, recipeId: Long) {
-        TODO()
+        if (!courseRepository.existsById(courseId)) throw ModelNotFoundException("Course", courseId)
+        val recipe =
+            recipeRepository.findByCourseIdAndId(courseId, recipeId) ?: throw ModelNotFoundException("Recipe", recipeId)
+
+        recipeRepository.delete(recipe)
     }
 }
