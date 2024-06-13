aae77b65a847afc72280acd83a1dd0f3b028139c
moomoo <anwns1211@gmail.com>
2024-06-13 16:38:34 +0900

    :sparkles: Feat: CourseResponseDto 추가

--- src/main/kotlin/team/sparta/onehouronemeal/domain/admin/controller/v1/AdminController.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/admin/controller/v1/AdminController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.sparta.onehouronemeal.domain.admin.service.v1.AdminService
-import team.sparta.onehouronemeal.domain.course.dto.v1.CourseResponse
+import team.sparta.onehouronemeal.domain.course.dto.v1.PendingCourseResponse
 import team.sparta.onehouronemeal.domain.user.dto.v1.UserResponse
 
 @RestController
@@ -36,7 +36,7 @@ class AdminController(
     }
 
     @GetMapping("/courses/pending")
-    fun getPendingCourseList(): ResponseEntity<List<CourseResponse>> {
+    fun getPendingCourseList(): ResponseEntity<List<PendingCourseResponse>> {
         return ResponseEntity.ok(adminService.getPendingCourseList())
     }
 
--- src/main/kotlin/team/sparta/onehouronemeal/domain/course/controller/v1/CourseController.kt
+++ src/main/kotlin/team/sparta/onehouronemeal/domain/course/controller/v1/CourseController.kt
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.sparta.onehouronemeal.domain.course.dto.v1.CourseResponse
+import team.sparta.onehouronemeal.domain.course.dto.v1.CourseResponseWithRecipes
 import team.sparta.onehouronemeal.domain.course.dto.v1.CreateCourseRequest
 import team.sparta.onehouronemeal.domain.course.dto.v1.UpdateCourseRequest
 import team.sparta.onehouronemeal.domain.course.service.v1.CourseService
@@ -36,7 +37,7 @@ class CourseController(
     }
 
     @GetMapping("/{courseId}")
-    fun getCourse(@PathVariable courseId: Long): ResponseEntity<CourseResponse> {
+    fun getCourse(@PathVariable courseId: Long): ResponseEntity<CourseResponseWithRecipes> {
         return ResponseEntity.ok(courseService.getCourse(courseId))
     }
 
