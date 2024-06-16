0dc54e4ead3cdbb2dd6c5c429ae2a6f0903aa5ee
itssu <63135789+devitssu@users.noreply.github.com>
2024-06-16 14:38:28 +0900

    :heavy_plus_sign: 테스트 의존성 추가 (#84)
    ## 요약
    테스트 코드 작성을 위한 의존성 추가
    
    ## 작업 사항
    - kotest v5.8.1
    - mockk v1.13.8 : Mocking을 위해 추가
    - springmockk v4.0.2 : `@MockkBean` 사용을 위해 추가
    
    
    ---
    ### 해결한 이슈
    closes: #83

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,8 @@ allOpen {
 
 val jjwtVersion = "0.12.5"
 val queryDslVersion = "5.0.0"
+val kotestVersion = "5.8.1"
+val mockkVersion = "1.13.8"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -59,7 +61,11 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
@@ -74,6 +80,6 @@ kotlin {
     }
 }
 
-tasks.withType<Test> {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
